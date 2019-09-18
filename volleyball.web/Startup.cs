52f136ce13f04c0e0a9754487a8047e1fa3edb26
Philip Kellner <philipkellner@Philips-Air.hsd1.il.comcast.net>
2019-09-17 22:56:27 -0500

    Changes for osx

--- volleyball.web/Startup.cs
+++ volleyball.web/Startup.cs
@@ -33,7 +33,7 @@ namespace volleyball.web
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +46,7 @@ namespace volleyball.web
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
